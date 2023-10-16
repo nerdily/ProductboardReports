ad3e7f7ed7a8b6243f5199f379efbb6e858f63cf
nerdily <bmwftw@icloud.com>
2023-10-16 10:49:01 -0400

    Export all notes in ProductBoard

---
+++ notes/get-all-notes.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import requests
+import sys
+import argparse
+import time
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="get-all-notes.py", description="Query ProductBoard for a list of all notes")
+    requiredNamed = parser.add_argument_group('required arguments')
+    requiredNamed.add_argument("-t", "--token", required=True,help="JWT bearer token used for authentication")
+    args = parser.parse_args()
+
+    req_url = 'https://api.productboard.com/notes?pageLimit=2000'
+    headers = {
+        "X-Version": "1",
+        "Authorization": "Bearer " f"{args.token}"
+    }
+    all_notes_df = pd.DataFrame()  # set an initial empty dataframe to append each customer data pull to
+
+    # Initial retrieval of notes
+    response = requests.request("GET", req_url, headers=headers)
+    if response.status_code == 200:
+        timestamp = time.strftime("%Y%m%d-%H%M%S")  # create a timestamp
+        print(timestamp + f": Notes list success {response}")
+        response = response.json()
+        notes_dict = response['data']
+        cursor = response['pageCursor']  # This API uses a 'cursor' to paginate through results. Grab it, and add it to the next_url
+        next_url = f"{req_url}" + "&pageCursor=" + cursor
+        notes_df = pd.DataFrame.from_dict(notes_dict)
+        all_notes_df = pd.concat([all_notes_df, notes_df], ignore_index=True)
+
+        while response['data'].__str__() != '[]':
+            response = requests.request("GET", next_url, headers=headers)
+            if response.status_code == 200:
+                timestamp = time.strftime("%Y%m%d-%H%M%S")  # create a timestamp
+                print(timestamp + f": Notes list success {response}")
+                response = response.json()
+                notes_dict = response['data']
+                cursor = response['pageCursor']
+                next_url = f"{req_url}" + "&pageCursor=" + cursor
+                notes_df = pd.DataFrame.from_dict(notes_dict)
+                all_notes_df = pd.concat([all_notes_df, notes_df], ignore_index=True)
+            else:
+                print(response)
+
+        # Cool. Export the dataframe to a CSV. Everyone wants a CSV
+        print("Done fetching notes")
+        timestamp = time.strftime("%Y%m%d-%H%M%S")  # create a timestamp
+        all_notes_df.to_csv('notes-' + timestamp + '.csv')
+        print('Saved to \'notes-' + timestamp + '.csv')
+
+    else:
+        print(response)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
